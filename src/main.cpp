f85a24d85886819fcfbb76ab50cd6c309c03fc0d
Tahi-knct <ynagiz22@gmail.com>
2022-12-08 19:03:10 +0900

    add switch device

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <esp_now.h>
 #include <WiFi.h>
+#include <esp_now.h>
 
 #include "config.h"
 
@@ -11,30 +11,30 @@ float QuatDataArray[20];
 
 void OnDataRecv(const uint8_t* mac, const uint8_t* recvData, int len) {
     for (size_t i = 0; i < ConnectDevice_Num; i++) {
-        if (recvData[0] == i) {
-            memcpy(&QuatDataArray[i * 4], recvData, len);
+        // Serial.println(*((float*)(recvData)), DEC);
+        if (*((float*)(recvData)) == i) {
+            memcpy(&QuatDataArray[i * 5], recvData, len);  // recvDataの生データだとよくわからない値がくるので
         }
     }
 
     Serial.print("s\t");
-    for (size_t i = 0; i < sizeof(QuatDataArray)/sizeof(float); i++) {
+    for (size_t i = 0; i < sizeof(QuatDataArray) / sizeof(float); i++) {
         Serial.print(QuatDataArray[i]);
         Serial.print("\t");
     }
     Serial.print("\n");
+    delay(1);  // watchdog_timerのリセット
 }
 
 void setup() {
-  Serial.begin(115200);
-  WiFi.mode(WIFI_AP);
-  WiFi.disconnect();
-  if (esp_now_init() != ESP_OK)  {
-    Serial.println("ESP-Now Init Failed....");
-    return;
-  }
-  esp_now_register_recv_cb(OnDataRecv);
+    Serial.begin(115200);
+    WiFi.mode(WIFI_AP);
+    WiFi.disconnect();
+    if (esp_now_init() != ESP_OK) {
+        Serial.println("ESP-Now Init Failed....");
+        return;
+    }
+    esp_now_register_recv_cb(OnDataRecv);
 }
 
-
-void loop() {
-}
+void loop() {}
